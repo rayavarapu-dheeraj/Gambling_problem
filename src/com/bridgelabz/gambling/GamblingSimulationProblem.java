7eb5780f846e6bf0f0018c293e4bd100421924be
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-08-13 09:43:05 +0530

    added: the first bet:

--- src/com/bridgelabz/gambling/GamblingSimulationProblem.java
+++ src/com/bridgelabz/gambling/GamblingSimulationProblem.java
@@ -3,7 +3,7 @@ package com.bridgelabz.gambling;
 public class GamblingSimulationProblem {
 	final static int STAKE = 100, BET = 1;
 	
-	public static void bet() {
+	public static void winOrLoose() {
 		final int random = (int) (Math.random() * 2);
 		if (random == 0) 
 			System.out.println("the gambler won ");
@@ -13,6 +13,6 @@ public class GamblingSimulationProblem {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulation Problem");
-		bet();
+		winOrLoose();
 	}
 }

0a50e79339f7ea288942dd1efaf7bafca2b96d88
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-08-13 08:32:02 +0530

    added: welcome message:

---
+++ src/com/bridgelabz/gambling/GamblingSimulationProblem.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.gambling;
+
+public class GamblingSimulationProblem {
+	public static void main(String[] args) {
+		System.out.println("Welcome to gambling Simulation Problem :");
+	}
+
+}
